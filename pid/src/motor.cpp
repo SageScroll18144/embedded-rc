3ba6f465cb9dadc4b4b3d3992a784302f539d147
SageScroll18144 <ffss@cin.ufpe.br>
2023-03-21 12:56:13 -0300

    estrutura do código do motor

--- pid/src/motor.cpp
+++ pid/src/motor.cpp
@@ -1,2 +1,5 @@
 #include "mbed.h"
 #include "motor.h"
+
+Motor::Motor(int pin_motor, int velocity);
+void Motor::setVelocity(int motor, int value);
--- pid/src/motor.h
+++ pid/src/motor.h
@@ -1,1 +1,11 @@
 #include "mbed.h"
+
+class Motor{
+    public:
+        Motor(int pin_motor, int velocity);
+        void setVelocity(int motor, int value);
+        void getVelocity(void);
+    private:
+        int velocity;
+        int pin_motor;
+};
